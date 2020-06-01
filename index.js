8792adc04f29f6e67516413945c1fc884801f6da
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-06-02 11:25:59 +1200

    Set Method, Get Method added

--- index.js
+++ index.js
@@ -40,13 +40,30 @@ class SinglyLinkedList {
         }
         return current;
     }
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter != index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, item) {
+        var foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.key = item;
+            return true;
+        }
+        return false;
+    }
     print() {
         this.pointer = this.head;
         for (let i = 0; i < this.length; i++) {
-            console.log(this.pointer.key + " -> ");
+            console.log(i + 1 + ". " + this.pointer.key);
             this.pointer = this.pointer.next;
         }
-        console.log("null")
     }
 }
 
@@ -57,5 +74,6 @@ toDoList.add("DES Assessment 2");
 toDoList.add("SDV Labs & Assessment 3");
 toDoList.add("Shopping");
 toDoList.pop();
+toDoList.set(2, "NOT DOING DES ASSESSMENT 2")
 
 toDoList.print();
